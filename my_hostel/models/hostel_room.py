024e19684887a293c6063ccf0260a51b6eaaafc4
sau-dla <sauhenriquez02@gmail.com>
2024-12-23 15:32:40 -0400

    [INP] - agrege nuevos registros a data.xml y cree un archivo 	 demo xml lo agregue a manifest, agregue un metodo de 	busqueda a hostel room

--- my_hostel/models/hostel_room.py
+++ my_hostel/models/hostel_room.py
@@ -7,7 +7,7 @@ class HostelRoom(models.Model):
     _name = "hostel.room"
     _inherit = ['base.archive']
     _description = "Hostel Room Information" 
-    _rec_name_search = ['room name', 'room state', 'rent amount']
+    _rec_name_search = ['room name']
     
     room_name = fields.Char('Room Name')
 
@@ -161,8 +161,15 @@ class HostelRoom(models.Model):
     def sort_rooms_by_rating(self, rooms):
         return rooms.sorted(key='room_rating')
 
+    # @api.model
+    # def create(self, values):
+    #     if not self.env.user.has_group('my_hostel.group_hostel_manager') and values.get('remarks'):
+    #         raise UserError("You are not authorized to modify remarks")
+    #     return super(HostelRoom, self).create(values)
+
     @api.model
-    def create(self, values):
-        if not self.env.user.has_group('my_hostel.group_hostel_manager') and values.get('remarks'):
-            raise UserError("You are not authorized to modify remarks")
-        return super(HostelRoom, self).create(values)
+    def _rec_names_search(self, name, args=None, operator='ilike', limit=100):
+        records = self.browse([]) 
+        if name:
+            records = self.search([('display_name', operator, name)], limit=limit)
+        return records.name_get()
