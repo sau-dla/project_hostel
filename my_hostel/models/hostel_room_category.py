604f36c0845e02684be590bf822818a7cfbeeec7
sau-dla <sauhenriquez02@gmail.com>
2025-01-18 16:55:34 -0400

    [IMP] - agregue alguno de los modelos a un sub menu 	le agregue una vista Calendar a hostel room category 	le agregue una vista Pivot a hostel room category 	le agregue una vista Graph a hostel room category

--- my_hostel/models/hostel_room_category.py
+++ my_hostel/models/hostel_room_category.py
@@ -6,6 +6,8 @@ class RoomCategory(models.Model):
     _description = "Hostel Room Category"
 
     name = fields.Char('Category')
+    date_assign = fields.Date('Date Assigned')
+    date_end = fields.Date(string='End Date')
     description = fields.Text('Description')
     parent_id = fields.Many2one(
         'hostel.room.category',
